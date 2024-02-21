692262ac4e78a6a68928c1490568386837b8a4c4
peterabrahamdev <peterabrahamdev@gmail.com>
2024-02-21 12:56:16 +0100

    chore: add comment on class functionality

--- lib/data/data_provider/weather_data_provider.dart
+++ lib/data/data_provider/weather_data_provider.dart
@@ -1,21 +1,24 @@
+// In this class we are going to the API and getting the raw data,
+// which we will convert in the repository class.
+
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:bloc_weather_app/secrets.dart';
 
-Future<String> getCurrentWeather(String cityName) async {
-  try {
-    String cityName = 'London';
-    final res = await http.get(
-      Uri.parse(
-        'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey',
-      ),
-    );
+class WeatherDataProvider {
+  Future<String> getCurrentWeather(String cityName) async {
+    try {
+      String cityName = 'London';
+      final res = await http.get(
+        Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey',
+        ),
+      );
 
-
-
-    return res.body;
-  } catch (e) {
-    throw e.toString();
+      return res.body;
+    } catch (e) {
+      throw e.toString();
+    }
   }
 }
