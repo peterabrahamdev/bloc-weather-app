8a7d9e54a478f3ad5cde33c2d04be37e89b32c00
peterabrahamdev <peterabrahamdev@gmail.com>
2024-02-21 12:43:01 +0100

    feat: create weather_model.dart

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,14 @@
+class WeatherModel {
+  final double currentTemp;
+  final String currentSky;
+  final double currentPressure;
+  final double currentWindSpeed;
+  final double currentHumidity;
+  WeatherModel({
+    required this.currentTemp,
+    required this.currentSky,
+    required this.currentPressure,
+    required this.currentWindSpeed,
+    required this.currentHumidity,
+  });
+}
